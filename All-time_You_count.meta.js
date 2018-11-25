98566386940b467ed25746ab1d8e2155202e5ef3
Kevin Parnell <kpbballer@gmail.com>
2018-11-25 12:19:05 -0500

    Create All-time_You_count.meta.js

---
+++ All-time_You_count.meta.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name        All-time (You) count
+// @namespace   com.KevinParnell.alltimeyoucount
+// @description All-time (You) count
+// @include     http*://boards.4chan.org/*
+// @exclude     http*://boards.4chan.org/*/catalog
+// @version     1.1
+// @match *://boards.4chan.org/*
+// @match *://boards.4channel.org/*
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_listValues
+// @grant       GM_deleteValue
+// @run-at      document-end
+// @updateURL   https://github.com/KevinParnell/All-time-You-count/raw/master/All-time_You_count.meta.js
+// @downloadURL https://github.com/KevinParnell/All-time-You-count/raw/master/All-time_You_count.user.js
+// ==/UserScript==
